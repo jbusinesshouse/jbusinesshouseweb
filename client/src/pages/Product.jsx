e9e3d2da775f22f03c158e333af816e7e1594e9f
tajbirhossain <mdtajbirhossain031@gmail.com>
2025-02-09 14:53:11 +0600

    Updated single product page to show slider

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -4,13 +4,20 @@ import Header from '../components/Header'
 import product1 from '../assets/images/panjabi1.jpeg'
 import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Thumbs, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/thumbs";
+import "swiper/css/navigation";
 
 const Product = () => {
     const [productData, setProductData] = useState({})
+    const [productImages, setProductImages] = useState([])
     const [quantity, setQuantity] = useState(1)
     const [selectedSize, setSelectedSize] = useState("")
     const [storeInfo, setStoreInfo] = useState({})
     const productId = window.location.pathname.split("/product/")[1]
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
     const navigate = useNavigate()
 
@@ -20,11 +27,12 @@ const Product = () => {
             behavior: 'smooth'
         });
         axios.get(`${process.env.REACT_APP_API_KEY}/product/getSingle/${productId}`).then(res => {
-            console.log(res.data);
+            // console.log(res.data);
             setProductData(res.data)
+            setProductImages([res.data.image, ...res.data.subImages])
             if (res.status === 200) {
                 axios.get(`${process.env.REACT_APP_API_KEY}/user/getUser/${res.data.storeId}`).then(res => {
-                    console.log(res.data);
+                    // console.log(res.data);
                     setStoreInfo(res.data)
                 })
             }
@@ -32,6 +40,8 @@ const Product = () => {
             console.log(err);
         })
     }, [productId])
+    console.log(productImages);
+
 
     const quantityDec = () => {
         if (quantity > 1) {
@@ -59,13 +69,53 @@ const Product = () => {
         }
     }
 
+    const images = [
+        "https://via.placeholder.com/600x400/ff7f7f/333333?text=Slide+1",
+        "https://via.placeholder.com/600x400/77ff77/333333?text=Slide+2",
+        "https://via.placeholder.com/600x400/7777ff/333333?text=Slide+3",
+        "https://via.placeholder.com/600x400/ffff77/333333?text=Slide+4",
+    ];
+
     return (
         <div className='product'>
             <Header />
             <section className='singleProductWrap'>
                 <div className="container">
                     <div className="productLeft">
-                        <img src={`${process.env.REACT_APP_API_KEY}/uploads/${productData.image}`} alt="" width="500" />
+                        {/* <img src={`${process.env.REACT_APP_API_KEY}/uploads/${productData.image}`} alt="" className='bigImg' /> */}
+                        <Swiper
+                            style={{ width: "100%", padding: "5px", border: "1px solid #ccc" }}
+                            spaceBetween={10}
+                            slidesPerView={1}
+                            thumbs={{ swiper: thumbsSwiper }}
+                            modules={[Thumbs, Navigation]}
+                            navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }}
+                            className='firstSlider'
+                        >
+                            {productImages.map((img, index) => (
+                                <SwiperSlide key={index}>
+                                    <img src={`${process.env.REACT_APP_API_KEY}/uploads/${img}`} alt={`Slide ${index + 1}`} className='bigImg' />
+                                </SwiperSlide>
+                            ))}
+                            <div className="swiper-button-prev"></div>
+                            <div className="swiper-button-next"></div>
+                        </Swiper>
+
+
+                        <Swiper
+                            onSwiper={setThumbsSwiper}
+                            spaceBetween={10}
+                            slidesPerView={5}
+                            watchSlidesProgress
+                            modules={[Thumbs]}
+                            style={{ marginTop: "10px", width: "100%" }}
+                        >
+                            {productImages.map((img, index) => (
+                                <SwiperSlide key={index} style={{ cursor: "pointer" }}>
+                                    <img src={`${process.env.REACT_APP_API_KEY}/uploads/${img}`} alt={`Thumb ${index + 1}`} style={{ width: "100%", height: "80px", objectFit: "cover" }} className='smallImg' />
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
                     </div>
                     <div className="productRight">
                         <Link to={`/seller/${storeInfo.id}`}>
